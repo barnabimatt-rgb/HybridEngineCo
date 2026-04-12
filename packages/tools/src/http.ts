b2369998be14d7a36c0d95010650dfb5cae174df
Matthew Barnabi <barnabimatt@gmail.com>
2026-04-11 21:22:32 -0700

    Update http.ts

--- packages/tools/src/http.ts
+++ packages/tools/src/http.ts
@@ -1,1 +1,10 @@
+import fetch, { RequestInit } from "node-fetch";
 
+export async function httpRequest(
+  url: string,
+  options: RequestInit = {}
+): Promise<{ status: number; body: string }> {
+  const res = await fetch(url, options);
+  const body = await res.text();
+  return { status: res.status, body };
+}
