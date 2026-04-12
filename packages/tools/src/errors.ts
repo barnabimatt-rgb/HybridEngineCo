bc4b8f76cb49c4653189cfc76453c9c5f1c7a275
Matthew Barnabi <barnabimatt@gmail.com>
2026-04-11 22:38:49 -0700

    Create errors.ts

---
+++ packages/tools/src/errors.ts
@@ -0,0 +1,10 @@
+export class HECError extends Error {
+  code: string;
+  details?: any;
+
+  constructor(code: string, message: string, details?: any) {
+    super(message);
+    this.code = code;
+    this.details = details;
+  }
+}
