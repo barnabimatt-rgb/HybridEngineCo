b5192af2b52e35601adb2fb139bef50ff1d9ad87
Matthew Barnabi <barnabimatt@gmail.com>
2026-04-11 21:41:33 -0700

    Create content.ts

---
+++ apps/api/src/routes/content.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { runPipeline } from "@hec/core";
+import { contentPipeline } from "@hec/core/src/pipeline/pipelines/contentPipeline";
+import { createLogger } from "@hec/tools";
+
+const router = Router();
+
+router.post("/content", async (req, res) => {
+  const logger = createLogger("api:content-pipeline");
+
+  const result = await runPipeline(contentPipeline, req.body || {}, {
+    runId: Date.now().toString(),
+    logger
+  });
+
+  res.json(result);
+});
+
+export default router;
