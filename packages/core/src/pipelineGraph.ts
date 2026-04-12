ca7950d9e4faa6251a28e1716b48b625a7f6dc78
Matthew Barnabi <barnabimatt@gmail.com>
2026-04-11 22:47:13 -0700

    Create pipelineGraph.ts

---
+++ packages/core/src/pipelineGraph.ts
@@ -0,0 +1,11 @@
+export const pipelineGraph = [
+  "topic-agent",
+  "script-agent",
+  "metadata-agent",
+  "brand-agent",
+  "voice-agent",
+  "video-agent",
+  "clip-agent",
+  "thumbnail-agent",
+  "upload-agent"
+];
